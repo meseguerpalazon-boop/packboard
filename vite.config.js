b6e70e7af3218b59fcb363ecc0c9cd15c537964d
meseguerpalazon-boop <meseguerpalazon@gmail.com>
2026-08-09 12:50:44 +0200

    Update server base configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['nuts-emily-accessible-actors.trycloudflare.com'],
+    base: packboard,
   },
 })
